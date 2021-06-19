cf3e254b14ec1576d13867c0bba64055825395e9
Clemens Schmid <clemens@nevrome.de>
2021-06-19 23:41:37 +0200

    further thought for matrix operations - too tired to make this work now

--- src/Currycarbon/Types.hs
+++ src/Currycarbon/Types.hs
@@ -1,8 +1,14 @@
 module Currycarbon.Types where
 
+import           Data.Matrix
 import qualified Data.Vector as V
 
 data UncalC14 = UncalC14 Double Double
 
 newtype UncalPDF = UncalPDF (V.Vector (Double, Double)) 
     deriving Show
+
+newtype CalPDF = CalPDF (V.Vector (Double, Double)) 
+    deriving Show
+
+newtype CalCurveSegment = CalCurveSegment (Matrix Double)
