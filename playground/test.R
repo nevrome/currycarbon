1e716709a9b593810a64cfd233beb5a0a5933f83
Clemens Schmid <clemens@nevrome.de>
2022-03-01 18:38:08 +0100

    test code to understand an issue with the CalCurveMatrix output

--- playground/test.R
+++ playground/test.R
@@ -62,3 +62,23 @@ calpal <- c14bazAAR::get_calpal()
 calpal |> dplyr::select(c14age, c14std) |> dplyr::slice_head(n = 5000) |> readr::write_csv("/tmp/currycarbon_large_input_test.csv", col_names = F)
 
 #currycarbon --inputFile /tmp/currycarbon_large_input_test.csv -q --densityFile /dev/null
+
+#### other tests ####
+
+run_currycarbon("--calCurveMatrixFile /tmp/curryMatrix.csv")
+
+cal_matrix <- readr::read_csv("/tmp/curryMatrix.csv") |>
+  tidyr::gather(vars, count, -...1) |>
+  dplyr::transmute(
+    uncal = ...1,
+    cal = as.numeric(vars),
+    val = count
+  )
+
+cal_matrix |> ggplot() +
+  geom_raster(
+    aes(x = cal, y = uncal, fill = val)
+  ) +
+  scale_y_reverse()
+
+  
