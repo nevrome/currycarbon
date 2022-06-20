8a7af699a3866f627af18396bdb9611e33dcad2a
Clemens Schmid <clemens@nevrome.de>
2022-06-20 21:55:34 +0200

    more efficient algorithm to fill the pdf ranges

--- src/Currycarbon/SumCalibration.hs
+++ src/Currycarbon/SumCalibration.hs
@@ -44,20 +44,27 @@ multiplyPDFs = combinePDFs (*) 1
 -- Combine probability densities
 combinePDFs :: (Float -> Float -> Float) -> Float -> CalPDF -> CalPDF -> CalPDF
 combinePDFs f initVal (CalPDF name1 cals1 dens1) (CalPDF name2 cals2 dens2) =
-        let --emptyRange = [(VU.last cals1+1)..(VU.head cals2-1)] ++ [(VU.last cals2+1)..(VU.head cals1-1)]
-            emptyRange = [(minimum [VU.minimum cals1, VU.minimum cals2])..(maximum [VU.maximum cals1, VU.maximum cals2])]
-            --pdfEmpty = zip emptyRange (repeat (0 :: Float))
-            empty1 = filter (\x -> not $ x `VU.elem` cals1) emptyRange
-            empty2 = filter (\x -> not $ x `VU.elem` cals2) emptyRange
-            c1 = VU.toList (VU.zip cals1 dens1) ++ zip empty1 (repeat (0 :: Float))
-            c2 = VU.toList (VU.zip cals2 dens2) ++ zip empty2 (repeat (0 :: Float))
-            --pdfValues = VU.toList (VU.zip cals1 dens1 VU.++ VU.zip cals2 dens2)
+        let minC1 = VU.minimum cals1
+            minC2 = VU.minimum cals2
+            maxC1 = VU.maximum cals1
+            maxC2 = VU.maximum cals2
+            emptyC1 = getMiss minC1 maxC1 minC2 maxC2
+            emptyC2 = getMiss minC2 maxC2 minC1 maxC1
+            c1 = VU.toList (VU.zip cals1 dens1) ++ zip emptyC1 (repeat (0 :: Float))
+            c2 = VU.toList (VU.zip cals2 dens2) ++ zip emptyC2 (repeat (0 :: Float))
             pdfSorted = sortBy (comparing fst) (c1 ++ c2)
             pdfGrouped = groupBy (\a b -> fst a == fst b) pdfSorted
-            pdfRes = map (\oneYear -> (head $ map fst oneYear, foldl' f initVal $ map snd oneYear)) pdfGrouped
-            pdfFin = sortBy (comparing fst) pdfRes
-            pdfNew = CalPDF (name1 ++ ":" ++ name2) (VU.fromList $ map fst pdfRes) (VU.fromList $ map snd pdfRes)
-        in  pdfNew -- error $ show pdfFin
+            pdfRes = map foldYearGroup pdfGrouped
+        in CalPDF (name1 ++ ":" ++ name2) (VU.fromList $ map fst pdfRes) (VU.fromList $ map snd pdfRes)
+        where 
+            getMiss :: YearBCAD -> YearBCAD -> YearBCAD -> YearBCAD -> [YearBCAD]
+            getMiss a1 a2 b1 b2
+                | a1 <  b1 && a2 >  b2 = [a1..b1] ++ [b2..a2]
+                | a1 <  b1 && a2 <= b2 = [a1..b1]
+                | a1 >= b1 && a2 >  b2 = [b2..a2]
+                | otherwise = []
+            foldYearGroup :: [(YearBCAD, Float)] -> (YearBCAD, Float)
+            foldYearGroup oneYear = (fst $ head oneYear, foldl' f initVal $ map snd oneYear)
 
 
 -- -- Combine probability densities
