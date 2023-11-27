0bc0c2c36633c31705ec3897e52185fee4260d68
Clemens Schmid <clemens@nevrome.de>
2023-11-27 10:30:03 +0100

    more test

--- test/ParserSpec.hs
+++ test/ParserSpec.hs
@@ -9,12 +9,18 @@ spec :: Spec
 spec = do
   testReadNamedExpression
 
-testUncalC14 :: String -> CalExpr
-testUncalC14 s = UnCalDate (UncalC14 s 3000 30)
-testWindowBP :: String -> CalExpr
-testWindowBP s = WindowBP (TimeWindowBP s 3000 2000)
-testWindowBCAD :: String -> CalExpr
-testWindowBCAD s = WindowBCAD (TimeWindowBCAD s (-1050) (-50))
+uncalC14N :: String -> CalExpr
+uncalC14N s = UnCalDate (UncalC14 s 3000 30)
+windowBPN :: String -> CalExpr
+windowBPN s = WindowBP (TimeWindowBP s 3000 2000)
+windowBCADN :: String -> CalExpr
+windowBCADN s = WindowBCAD (TimeWindowBCAD s (-1050) (-50))
+uncalC14 :: CalExpr
+uncalC14 = uncalC14N ""
+windowBP :: CalExpr
+windowBP = windowBPN ""
+windowBCAD :: CalExpr
+windowBCAD = windowBCADN ""
 
 testReadNamedExpression :: Spec
 testReadNamedExpression =
@@ -22,30 +28,63 @@ testReadNamedExpression =
         it "should read uncalibrated C14 dates correctly" $ do
             readOneNamedCalExpr "3000,30"
                 `shouldBe`
-                Right (NamedCalExpr "" (testUncalC14 ""))
+                Right (NamedCalExpr "" uncalC14)
             readOneNamedCalExpr "uncalC14(3000,30)"
                 `shouldBe`
-                Right (NamedCalExpr "" (testUncalC14 ""))
+                Right (NamedCalExpr "" uncalC14)
             readOneNamedCalExpr "uncalC14(test,3000,30)"
                 `shouldBe`
-                Right (NamedCalExpr "" (testUncalC14 "test"))
+                Right (NamedCalExpr "" (uncalC14N "test"))
         it "should read time windows correctly" $ do
             readOneNamedCalExpr "rangeBP(3000,2000)"
                 `shouldBe`
-                Right (NamedCalExpr "" (testWindowBP ""))
+                Right (NamedCalExpr "" windowBP)
             readOneNamedCalExpr "rangeBCAD(-1050,-50)"
                 `shouldBe`
-                Right (NamedCalExpr "" (testWindowBCAD ""))
+                Right (NamedCalExpr "" windowBCAD)
             readOneNamedCalExpr "rangeBP(test,3000,2000)"
                 `shouldBe`
-                Right (NamedCalExpr "" (testWindowBP "test"))
+                Right (NamedCalExpr "" (windowBPN "test"))
             readOneNamedCalExpr "rangeBCAD(test,-1050,-50)"
                 `shouldBe`
-                Right (NamedCalExpr "" (testWindowBCAD "test"))
-        it "should read simple sums correctly " $ do
+                Right (NamedCalExpr "" (windowBCADN "test"))
+        it "should read sums with + operator correctly " $ do
             readOneNamedCalExpr "uncalC14(3000,30) + rangeBP(3000,2000)"
                 `shouldBe`
-                Right (NamedCalExpr "" $ SumCal (testUncalC14 "") (testWindowBP ""))
-            readOneNamedCalExpr "uncalC14(3000,30) + rangeBP(3000,2000) + range"
+                Right (NamedCalExpr "" $ SumCal uncalC14 windowBP)
+            readOneNamedCalExpr "uncalC14(3000,30) + rangeBP(3000,2000) + rangeBCAD(-1050,-50)"
                 `shouldBe`
-                Right (NamedCalExpr "" $ SumCal (testUncalC14 "") (testWindowBP ""))
+                Right (NamedCalExpr "" $ SumCal uncalC14 (SumCal windowBP windowBCAD))
+            readOneNamedCalExpr "uncalC14(3000,30) + rangeBP(3000,2000) + rangeBCAD(-1050,-50) + uncalC14(3000,30)"
+                `shouldBe`
+                Right (NamedCalExpr "" $ SumCal uncalC14 (SumCal windowBP (SumCal windowBCAD uncalC14)))
+        it "should read sums with sum() function and + operator correctly " $ do    
+            readOneNamedCalExpr "sum(uncalC14(3000,30), rangeBP(3000,2000))"
+                `shouldBe`
+                Right (NamedCalExpr "" $ SumCal uncalC14 windowBP)
+            readOneNamedCalExpr "sum(uncalC14(3000,30), rangeBP(3000,2000)) + rangeBCAD(-1050,-50)"
+                `shouldBe`
+                Right (NamedCalExpr "" $ SumCal (SumCal uncalC14 windowBP) windowBCAD)
+            readOneNamedCalExpr "uncalC14(3000,30) + sum(rangeBP(3000,2000), rangeBCAD(-1050,-50)) + uncalC14(3000,30)"
+                `shouldBe`
+                Right (NamedCalExpr "" $ SumCal uncalC14 (SumCal (SumCal windowBP windowBCAD) uncalC14))
+        it "should read products with * operator correctly " $ do
+            readOneNamedCalExpr "uncalC14(3000,30) * rangeBP(3000,2000)"
+                `shouldBe`
+                Right (NamedCalExpr "" $ ProductCal uncalC14 windowBP)
+            readOneNamedCalExpr "uncalC14(3000,30) * rangeBP(3000,2000) * rangeBCAD(-1050,-50)"
+                `shouldBe`
+                Right (NamedCalExpr "" $ ProductCal uncalC14 (ProductCal windowBP windowBCAD))
+            readOneNamedCalExpr "uncalC14(3000,30) * rangeBP(3000,2000) * rangeBCAD(-1050,-50) * uncalC14(3000,30)"
+                `shouldBe`
+                Right (NamedCalExpr "" $ ProductCal uncalC14 (ProductCal windowBP (ProductCal windowBCAD uncalC14)))
+        it "should read products with product() function and * operator correctly " $ do    
+            readOneNamedCalExpr "product(uncalC14(3000,30), rangeBP(3000,2000))"
+                `shouldBe`
+                Right (NamedCalExpr "" $ ProductCal uncalC14 windowBP)
+            readOneNamedCalExpr "product(uncalC14(3000,30), rangeBP(3000,2000)) * rangeBCAD(-1050,-50)"
+                `shouldBe`
+                Right (NamedCalExpr "" $ ProductCal (ProductCal uncalC14 windowBP) windowBCAD)
+            readOneNamedCalExpr "uncalC14(3000,30) * product(rangeBP(3000,2000), rangeBCAD(-1050,-50)) * uncalC14(3000,30)"
+                `shouldBe`
+                Right (NamedCalExpr "" $ ProductCal uncalC14 (ProductCal (ProductCal windowBP windowBCAD) uncalC14))
