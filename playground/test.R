389fb3d9af49df223d1313f46d1a8348310c599f
Clemens Schmid <clemens@nevrome.de>
2023-11-29 13:43:50 +0100

    fixed some details in the test script

--- playground/test.R
+++ playground/test.R
@@ -45,7 +45,7 @@ bchron <- tibble::tibble(
 bchron |> 
   dplyr::full_join(curry_bchron_studentT100, by = "calBCAD") |>
   dplyr::full_join(curry_bchron_normal, by = "calBCAD") |>
-  dplyr::full_join(curry_matrixmult_default, by = "calBCAD") |>
+  dplyr::full_join(curry_matrixmult, by = "calBCAD") |>
   tidyr::pivot_longer(
     tidyselect::starts_with("dens"),
     names_to = "method"
@@ -53,7 +53,7 @@ bchron |>
   ggplot() +
   geom_line(
     aes(x = calBCAD, y = value, colour = method), 
-    size = 1, alpha = 0.5
+    linewidth = 1, alpha = 0.5
   )
 
 #### large test (for memory leaks) ####
@@ -61,7 +61,7 @@ bchron |>
 calpal <- c14bazAAR::get_calpal()
 calpal |> dplyr::select(c14age, c14std) |> dplyr::slice_head(n = 5000) |> readr::write_csv("/tmp/currycarbon_large_input_test.csv", col_names = F)
 
-# currycarbon --inputFile /tmp/currycarbon_large_input_test.csv -q
+system("currycarbon --inputFile /tmp/currycarbon_large_input_test.csv -q")
 
 #### cal curve ####
 
