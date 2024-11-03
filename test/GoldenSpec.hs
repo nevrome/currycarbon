7db441f0332c4505717c618b841e6152c4850cff
Clemens Schmid <clemens@nevrome.de>
2024-11-03 15:35:22 +0100

    adjusted tests to this new behaviour

--- test/GoldenSpec.hs
+++ test/GoldenSpec.hs
@@ -16,7 +16,7 @@ goldenTest =
 
     let stdout_stderr_tests = [
               "single_radiocarbon_date"
-            , "cli_error_behaviour"
+            , "tricky_expressions"
             ]
 
     runStdoutStderrTests stdout_stderr_tests
