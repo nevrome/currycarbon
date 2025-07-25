5f3d8b85fec80ac8603bc22e1862a0c359ae426c
Clemens Schmid <nevromecs@gmail.com>
2025-07-25 09:58:49 +0200

    safe parsing mechanism for external calibration curves

--- src/Currycarbon/CalCurves.hs
+++ src/Currycarbon/CalCurves.hs
@@ -4,11 +4,13 @@ module Currycarbon.CalCurves where
 
 import           Currycarbon.ParserHelpers
 import           Currycarbon.Types
+import Currycarbon.Utils
 
 import qualified Data.FileEmbed            as FE
 import qualified Data.Vector.Unboxed       as VU
 import qualified Text.Parsec               as P
 import qualified Text.Parsec.String        as P
+import Control.Exception (throwIO)
 
 data CalCurveSelection =
       IntCal20 | SHCal20 | Marine20
@@ -22,7 +24,7 @@ instance Show CalCurveSelection where
 
 readCalCurveSelection :: String -> Either String CalCurveSelection
 readCalCurveSelection s =
-    case P.runParser parseCalCurveSelection () "" s of
+    case P.runParser parseCalCurveSelection () s s of
         Left err -> Left $ showParsecErr err
         Right x  -> Right x
 
@@ -44,14 +46,25 @@ getCalCurve (CalCurveFromFile path) = readCalCurveFromFile path
 -- | Read a calibration curve file. The file must adhere to the .14c file format.
 readCalCurveFromFile :: FilePath -> IO CalCurveBP
 readCalCurveFromFile calCurveFile = do
-    calCurve <- readFile calCurveFile
-    return $ readCalCurve calCurve
+    calCurveString <- readFile calCurveFile
+    case readCalCurve calCurveString of
+        Left err -> throwIO $ CurrycarbonCLIParsingException err
+        Right x -> return x
 
-readCalCurve :: String -> CalCurveBP
+readCalCurve :: String -> Either String CalCurveBP
 readCalCurve calCurveString = do
-    case P.runParser parseCalCurve () "" calCurveString of
-        Left p  -> error $ "This should never happen." ++ show p
-        Right x -> CalCurveBP
+    case P.runParser parseCalCurve () "calCurve" calCurveString of
+        Left err -> Left $ showParsecErr err
+        Right x  -> Right $ CalCurveBP
+            (VU.fromList $ map (\(a,_,_) -> a) x)
+            (VU.fromList $ map (\(_,b,_) -> b) x)
+            (VU.fromList $ map (\(_,_,c) -> c) x)
+
+readCalCurveUnsafe :: String -> CalCurveBP
+readCalCurveUnsafe calCurveString = do
+    case P.runParser parseCalCurve () "calCurve" calCurveString of
+        Left err -> error $ "Error when reading calCurve " ++ show err
+        Right x  -> CalCurveBP
             (VU.fromList $ map (\(a,_,_) -> a) x)
             (VU.fromList $ map (\(_,b,_) -> b) x)
             (VU.fromList $ map (\(_,_,c) -> c) x)
@@ -79,20 +92,20 @@ comments = do
 -- | The intcal20 calibration curve
 -- (Reimer et al. 2020, doi: [10.1017/RDC.2020.41](https://doi.org/10.1017/RDC.2020.41))
 intcal20 :: CalCurveBP
-intcal20 = readCalCurve intcal20String
+intcal20 = readCalCurveUnsafe intcal20String
 intcal20String :: String
 intcal20String = $(FE.makeRelativeToProject "data/intcal20.14c" >>= FE.embedStringFile)
 
 -- | The shcal20 calibration curve
 -- (Hogg et al. 2020, doi: [10.1017/RDC.2020.59](https://doi.org/10.1017/RDC.2020.59))
 shcal20 :: CalCurveBP
-shcal20 = readCalCurve shcal20String
+shcal20 = readCalCurveUnsafe shcal20String
 shcal20String :: String
 shcal20String = $(FE.makeRelativeToProject "data/shcal20.14c" >>= FE.embedStringFile)
 
 -- | The shcal20 calibration curve
 -- (Heaton et al. 2020, doi: [10.1017/RDC.2020.68](https://doi.org/10.1017/RDC.2020.68))
 marine20 :: CalCurveBP
-marine20 = readCalCurve marine20String
+marine20 = readCalCurveUnsafe marine20String
 marine20String :: String
 marine20String = $(FE.makeRelativeToProject "data/marine20.14c" >>= FE.embedStringFile)
